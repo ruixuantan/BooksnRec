86dc7a0eb5e9d8b4373bc5531deff1aa0beed081
ruixuantan <ruixuantan@yahoo.com.sg>
2023-10-10 17:47:19 +0800

    setup library database

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
+include .env
+
 up:
 	docker compose up --build -d
 
 down:
 	docker compose down
+
+libdb-shell:
+	docker-compose exec library-db psql -d ${LIBRARY_DB_DB} -U ${LIBRARY_DB_USER}
 
 .PHONY: library cassandra
---
+++ library-db/init.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS
+    Readers,
+    Books,
+    Loans,
+CASCADE;
+
+CREATE TABLE Readers(
+    id UUID PRIMARY KEY,
+    card_no CHAR(10) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    mobile_no CHAR(8) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    dob DATE,
+    registered_on DATE NOT NULL
+);
+
+CREATE TABLE Books(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    isbn10 CHAR(10) NOT NULL,
+    isbn13 CHAR(13) NOT NULL
+);
+
+CREATE TABLE Loans(
+    id BIGSERIAL PRIMARY KEY,
+    reader_id UUID NOT NULL,
+    book_id SERIAL NOT NULL,
+    reserve_date DATE,
+    loan_date DATE,
+    return_date DATE,
+    FOREIGN KEY (reader_id) REFERENCES Readers(id),
+    FOREIGN KEY (book_id) REFERENCES Books(id)
+);
